b0782c98948ca66ca4a6d2f50264099f0e9d2362
Julia Zapata <juliamzapata@gmail.com>
2018-09-03 21:42:25 -0300

    all test are done, created lib and .gitignore installed

--- index.js
+++ index.js
@@ -1,2 +1,47 @@
-// função do validador 
+module.exports.cardValidator = function cardValidator(cardNum){
+    if (cardNum === undefined){
+        throw new Error ("prencha o parâmetro da função");
+      }
+    if (typeof cardNum === "string"){
+        throw new Error ("o parametro precisa ser do tipo Number");
+    }
+     
+    
+    let reversedNum = cardNum.toString().split("").map((i) => {
+      return parseInt(i, 10)
+      }).reverse();
+      if (reversedNum.length === 1){
+        throw new Error ("não pode ter somente um digito");
+    }  
+    let even = [];
+    let oddTwo = [];
+    let odd = reversedNum.filter((a, index) =>{
+      if (index % 2 === 1){
+        return index
+      } else {
+          even.push(reversedNum[index]);}})
+          .map((a)=>{
+              return a * 2
+            }).filter((b)=>{
+                 if (b > 9){
+                 return b
+                 } else (
+                     oddTwo.push(b)
+                 )}).map((c)=>{
+                return c - 9 
+              });
+    let concatNums = odd.concat(even, oddTwo);
+    let  sum = 0;
+    concatNums.map((a)=>{
+    return sum += a  
+  }); 
+  if (sum %10 === 0){
+        return true;
+      } else {
+          return false;
+        }
+  if (card){
 
+  }      
+  }
+//   cardValidator(5293230347113647);
--- test-index.spec.js
+++ test-index.spec.js
@@ -1,3 +1,44 @@
+const mocha = require("mocha");
+const chai = require("chai");
+const validatorErrors = require("./index");
+const expect = chai.expect;
+
+describe("validatorErrors", ()=>{
+  describe("#validatorTests", ()=>{
+    describe("Quando nao houver parametro", ()=>{
+      it("deve me retornar um erro", ()=>{
+        var errorFunction = function () {validatorErrors.cardValidator()}
+      expect(errorFunction).to.throw("prencha o parâmetro da função")
+      });
+    });
+    describe("Quando o numero for uma string", ()=>{
+      it("deve me retornar um erro", ()=>{
+        var errorFunction = function () {validatorErrors.cardValidator("string")}
+      expect(errorFunction).to.throw("o parametro precisa ser do tipo Number")
+      });
+    });
+    describe("Quando o numero for um inteiro e houver um digito", ()=>{
+      it("deve me retornar um erro", ()=>{
+        var errorFunction = function () {validatorErrors.cardValidator(1)}
+      expect(errorFunction).to.throw("não pode ter somente um digito")
+      });
+    });
+    describe("Quando o numero for um inteiro e for um cartão válido", ()=>{
+      it("deve me retornar um true", ()=>{
+       expect(validatorErrors.cardValidator(5293230347113647)).to.equal(true);
+      });
+    });
+    describe("Quando o numero for um inteiro e for um cartão invalido", ()=>{
+      it("deve me retornar false", ()=>{
+      expect(validatorErrors.cardValidator(5293230347111647)).to.equal(false);
+      });
+    });
+  });
+})
+
+
+
+
 // Quando nao houver parametro deve lancar um erro.
 
 // Quando o numero for uma string deve lancar um erro.
