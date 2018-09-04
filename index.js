1e9a62b04c6f69f3361847f967695b79191d2f7b
Julia Zapata <juliamzapata@gmail.com>
2018-09-04 15:11:29 -0300

    usei o --fix do eslintrc para melhorar estilo

--- index.js
+++ index.js
@@ -6,41 +6,38 @@ module.exports.cardValidator = function cardValidator(cardNum){
         throw new Error ("o parametro precisa ser do tipo Number");
     }
     let reversedNum = cardNum.toString().split("")
-    .map((i) => {
-        return parseInt(i, 10)
-      }).reverse();
-      if (reversedNum.length === 1){
+        .map((i) => {
+            return parseInt(i, 10);
+        }).reverse();
+    if (reversedNum.length === 1){
         throw new Error ("não pode ter somente um digito");
     }  
     let even = [];
     let oddTwo = [];
     let odd = reversedNum.filter((a, index) =>{
-      if (index % 2 === 1){
-        return index
-      } else {
-          even.push(reversedNum[index]);}})
-          .map((a)=>{
-              return a * 2
-            }).filter((b)=>{
-                 if (b > 9){
-                 return b
-                 } else (
-                     oddTwo.push(b)
-                 )}).map((c)=>{
-                return c - 9 
-              });
+        if (index % 2 === 1){
+            return index;
+        } else {
+            even.push(reversedNum[index]);}})
+        .map((a)=>{
+            return a * 2;
+        }).filter((b)=>{
+            if (b > 9){
+                return b;
+            } else (
+                oddTwo.push(b)
+            );}).map((c)=>{
+            return c - 9; 
+        });
     let concatNums = odd.concat(even, oddTwo);
     let  sum = 0;
     concatNums.map((a)=>{
-    return sum += a  
-  }); 
-  if (sum %10 === 0){
+        return sum += a;  
+    }); 
+    if (sum %10 === 0){
         return true;
-      } else {
-          return false;
-        }
-  if (card){
-
-  }      
-  }
+    } else {
+        return false;
+    }
+};
 //   cardValidator(5293230347113647);
